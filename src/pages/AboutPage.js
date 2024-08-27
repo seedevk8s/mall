09fbda8689a75947a5981a6413a37664277346ed
seedevk8s <seedevk8s@gmail.com>
2024-08-27 23:41:54 +0900

    주석추가

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,10 @@
 
-function AboutPage(props) {
-  return (
-    <div className={'text-3xl'}>
+function AboutPage(props) {   //AboutPage는 함수형 컴포넌트로, props라는 인자를 받습니다. 
+  return (                    //현재 코드에서는 props를 사용하고 있지 않지만, 이는 컴포넌트에 전달되는 속성을 처리할 수 있는 방법을 제공함
+    <div className={'text-3xl'}>  
       <h1>About Page</h1>
     </div>
   );
-}
+}             //컴포넌트의 반환값은 JSX입니다. JSX는 JavaScript를 확장한 문법으로 React에서 UI를 표현하는 데 사용됨.
 
-export default AboutPage;
+export default AboutPage;     //AboutPage 컴포넌트를 기본값으로 내보내고 있음.
--- src/router/root.js
+++ src/router/root.js
@@ -1,21 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainPage from "../pages/MainPage";
-import { Suspense, lazy } from "react";
+//import MainPage from "../pages/MainPage";
+import { Suspense, lazy } from "react";     //React의 내장 함수로, 컴포넌트를 비동기적으로 불러오고 로딩 상태를 관리하는 데 사용
 
 const Loading = <div className={'bg-red-700'}>Loading...</div>;
-const Main = lazy(() => import("../pages/MainPage"));
-const About = lazy(() => import("../pages/AboutPage"));
+const Main = lazy(() => import("../pages/MainPage"));   //lazy 함수를 사용해 MainPage와 AboutPage 컴포넌트를 비동기적으로 불러옴. 
+const About = lazy(() => import("../pages/AboutPage")); //이 컴포넌트들은 필요할 때만 로드되어 초기 로딩 시간을 단축시킴.
 
 
-const root = createBrowserRouter([
+const root = createBrowserRouter([                  //createBrowserRouter 함수를 통해 라우터 구성을 설정
     {
         path: "",
-        element: <Suspense fallback={Loading}><Main/></Suspense>
+        element: <Suspense fallback={Loading}><Main/></Suspense>    //Suspense 컴포넌트는 자식 컴포넌트가 로드될 때까지 fallback으로 지정된 Loading 컴포넌트를 보여주게 함
     },
     {
-        path: "about",
+        path: "about",                              //각각의 라우트는 특정 경로(path)에 접근했을 때 보여질 요소(element)를 지정
         element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "main",
+        element: <Suspense fallback={Loading}><Main/></Suspense>
     }
-]);
+            //이 코드는 비동기적 페이지 로딩과 동적 라우팅을 구현하여 초기 로드 시 성능을 향상시키고, 사용자 경험을 개선합니다. 
+]);         //Suspense와 lazy의 조합은 리소스를 효율적으로 사용하면서도, 사용자에게 느린 로드 타임이 발생할 경우 적절한 피드백을 제공할 수 있게 함
 
 export default root;
