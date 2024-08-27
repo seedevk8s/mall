b9825d762b6db35dc917fa8f051cde363c9ff036
seedevk8s <seedevk8s@gmail.com>
2024-08-27 23:17:35 +0900

    about 라우터 추가

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+
+function AboutPage(props) {
+  return (
+    <div className={'text-3xl'}>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
--- src/router/root.js
+++ src/router/root.js
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/MainPage";
+import { Suspense, lazy } from "react";
+
+const Loading = <div className={'bg-red-700'}>Loading...</div>;
+const Main = lazy(() => import("../pages/MainPage"));
+const About = lazy(() => import("../pages/AboutPage"));
+
 
 const root = createBrowserRouter([
     {
-        path: '',
-        element: <MainPage />
+        path: "",
+        element: <Suspense fallback={Loading}><Main/></Suspense>
+    },
+    {
+        path: "about",
+        element: <Suspense fallback={Loading}><About/></Suspense>
     }
 ]);
 
