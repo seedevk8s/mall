fc40aa8b634bf3b519e85c1aa954e6fe7c7d0d86
seedevk8s <seedevk8s@gmail.com>
2024-08-28 23:33:23 +0900

    레이아웃 컴포넌트 생성하기

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,16 +1,12 @@
 import React from 'react';    //React를 불러옵니다.
 import { Link } from 'react-router-dom';
+import BasicLayout from '../layouts/BasicLayout';
 
 function AboutPage(props) {   //AboutPage는 함수형 컴포넌트로, props라는 인자를 받습니다. 
   return (                    //현재 코드에서는 props를 사용하고 있지 않지만, 이는 컴포넌트에 전달되는 속성을 처리할 수 있는 방법을 제공함
-    <div className={'text-3xl'}>  
-
-      <div>
-        <Link to={'/about'}>About</Link>
-      </div>
-
-      <h1>About Page</h1>
-    </div>
+    <BasicLayout>
+      <div className='text-3xl'>About Page</div>
+    </BasicLayout>
   );
 }             //컴포넌트의 반환값은 JSX입니다. JSX는 JavaScript를 확장한 문법으로 React에서 UI를 표현하는 데 사용됨.
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,11 @@
 import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 function MainPage() {
   return (
-    <div className={'text-3xl'}>
-
-      <div>
-        <Link to={'/about'}>About</Link>
-      </div>
-
-      <h1>Main Page</h1>
-    </div>
+    <BasicLayout>
+      <div className="text-3xl">Main Page</div>
+    </BasicLayout>
   );
 }
 
