8a1fe5d66b62cde94fbd4de4f259f01a6d3c2e53
seedevk8s <seedevk8s@gmail.com>
2024-09-01 21:39:29 +0900

    동적 페이지 이동 추가--read url에서 list 클릭시 page와 size 값 유지안됨

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { createSearchParams, useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 function ReadPage(props) {
 
     // const obj = useParams();
     // console.log(obj); // { id: "1" }
 
+    const navigate = useNavigate();
+
     const {tno} = useParams();
     console.log(tno); // 1
 
+    const moveToModify = (tno) => {
+        navigate({
+            pathname:`/todo/modify/${tno}`,
+            search: queryStr
+        })
+    }
+
+    const [queryParams] = useSearchParams();
+    const page = queryParams.get('page') ? parseInt(queryParams.get('page') ) : 1
+    const size = queryParams.get('size') ? parseInt(queryParams.get('size') ) : 10    
+
+    const queryStr = createSearchParams({page: page, size: size}).toString();
+
+
+    const moveToList = () => {
+        navigate({
+            pathname:`/todo/List`,
+            search: queryStr
+        })      
+    }
+
     return (
         <div className="text-3xl">
-            Todo Read Page Component
+            Todo Read Page Component {tno}
+
+            <div>
+                <button onClick={() => moveToModify(tno)}>Test Modify</button>
+                <button onClick={moveToList}>Test List</button>
+            </div>
         </div>
     );
 }
